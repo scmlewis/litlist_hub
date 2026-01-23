2854c785e777dbc72dfe9522e3c54de63045292f
scmlewis <scmlewis@gmail.com>
2026-01-23 18:30:17 +0800

    Remove unused setup.ts that causes build errors

--- lib/setup.ts
+++
@@ -1,6 +0,0 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-
-// Setup Cloudflare bindings in development
-if (process.env.NODE_ENV === 'development') {
-  setupDevPlatform();
-}
